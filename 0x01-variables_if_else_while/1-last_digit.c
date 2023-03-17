0090c64ec2e8694d62aa3051494614acf374d2dc
sangaryousmane <sangaryousmane@gmail.com>
2023-03-17 09:28:27 -0700

    The Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %d is %d and is 0\n ", n, (n % 10));
 }
 else 
 {
-printf("Last digit of %d is %d and less than 6 and not 0\n ", n, (n % 10));
+printf("Last digit of %d is %d and is less than 6 and not 0\n ", n, (n % 10));
 }
 return (0);
 }
