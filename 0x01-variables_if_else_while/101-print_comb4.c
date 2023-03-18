454cfe5892324b2baa234c98c645e08e6183908b
sangaryousmane <sangaryousmane@gmail.com>
2023-03-17 18:49:42 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,14 +14,17 @@ for (j = i + 1; j <= 10; j++)
 {
 for (k = j + 1; k <= 10; k++)
 {
-if (j == 9 && j == 8 && k == 7)
+putchar((i % 10) + '0');
+putchar((i % 10) + '0');
+putchar((i % 10) + '0');
+if (i == 7 && j == 8 && k == 9)
 {
 continue;
 }
-}
-}
 putchar(',');
 putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
