6c1d6c1f749bd6bfd89256789a60bf8cbf96ab40
sangaryousmane <sangaryousmane@gmail.com>
2023-03-21 15:40:49 -0700

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*add - adds two numbers
+*@num1: first number to pass to print_sign function
+*@num2: second number to pass to print_sign function
+*Return: the sum of @num1 and @num2
+*/
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
