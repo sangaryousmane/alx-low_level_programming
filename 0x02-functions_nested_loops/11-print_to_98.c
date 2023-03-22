aa59cec0204e1b812897025ce21528ebb49e2ac8
sangaryousmane <sangaryousmane@gmail.com>
2023-03-21 18:31:56 -0700

    Added Natural 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 
-
-
 void print_to_98(int n)
 {
+int i;
+i = 0;
 
-if (n <
+while (i <= n)
+{
+if (i != 98)
+{
+printf("%d, ", i);
+}
+else
+{
+printf("%d\n", i);
+}
+}
 }
