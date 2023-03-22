b2423ed40e0a3bff145e1091a094dc8a6b41c4ef
sangaryousmane <sangaryousmane@gmail.com>
2023-03-21 18:01:43 -0700

    Added fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,9 +13,13 @@ long i, num1 = 1, num2 = 2;
 long  temp, n = 50;
 for (i = 0; i < n; i++)
 {
-if (i != 50)
+if (i == 20365011074)
 {
 printf("%ld, ", num1);
+}
+else
+{
+printf("%ld\n", num1);
 }
 temp = num1 + num2;
 num1 = num2;
