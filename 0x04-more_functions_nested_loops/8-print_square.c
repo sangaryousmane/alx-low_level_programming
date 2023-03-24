f4348ea8b8c0ad8bafb34a6cae62a7baf853542f
sangaryousmane <sangaryousmane@gmail.com>
2023-03-23 18:54:30 -0700

    That square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_square - print square if @size is positive
+* @size: param to the function
+* Return nothing
+*/
+void print_square(int size)
+{
+int i, j;
+
+if (n > 0)
+{
+for (i = 0 ; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+_putchar(10);
+}
