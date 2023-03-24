54e4eb64761e95584e0c1f1261a1db9bfe74e970
sangaryousmane <sangaryousmane@gmail.com>
2023-03-23 19:49:43 -0700

    Fizbuzz done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - implement our fizzbuzz algorithm
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("%s ", "Fizzbuzz");
+}
+else if (i % 3 == 0)
+{
+printf("%s ", "Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("%s ", "Buzz");
+}
+
+else
+{
+printf("%d ", i);
+}
+}
+
+}
