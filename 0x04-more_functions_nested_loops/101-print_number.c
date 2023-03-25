b0b84f95f68f6446bead17a785852360cd9a3389
sangaryousmane <sangaryousmane@gmail.com>
2023-03-24 21:10:57 -0700

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+* print_number - print integers without using arrays, pointers or hard coding
+* @n: parameter to the print_number function
+* Return nothing
+*/
+void print_number(int n)
+{
+unsigned int num = n;
+if (num < 0)
+{
+_putchar('-');
+num = -num;
+}
+if (num > 9)
+{
+print_number(num / 10);
+}
+_putchar(num % 10);
+}
