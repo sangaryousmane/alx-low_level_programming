f373e8eb7dbc6b082400cbd55057fa27e12c56fc
sangaryousmane <sangaryousmane@gmail.com>
2023-03-27 06:39:52 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* reset_to_98 - reset value to 98
+* Return nothing
+* @n: takes an int and update it
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+int _putchar(int c);
+void reset_to_98(int *n);
