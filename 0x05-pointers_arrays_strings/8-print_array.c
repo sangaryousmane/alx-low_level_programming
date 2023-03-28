d44fe846e6beeaf818c59b7eb3792d0a0df2a27d
sangaryousmane <sangaryousmane@gmail.com>
2023-03-27 18:10:04 -0700

    Added array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,18 +10,16 @@
 void print_array(int *a, int n)
 {
 int i;
-int end;
-end = n - 1;
 
 for (i = 0; i < n; i++)
 {
-if (i != end)
+if (i != (n - 1))
 {
-printf("%d, ", *(a + i));
+printf("%d, ", a[i]);
 }
 else
 {
-printf("%d", *(a + i));
+printf("%d", a[i]);
 }
 }
 _putchar('\n');
