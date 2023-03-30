083473efd0443afc30125fbc2f8347ba9aca0a19
sangaryousmane <sangaryousmane@gmail.com>
2023-03-30 11:31:50 -0700

    Added Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,39 +1,24 @@
-#include <stdio.h>
 #include "main.h"
-/**
-* leet - Write a function that encodes a string into 1337.
-* @str: points at the string in memory
-* Return: a pointer to the string
-*/
-char *leet(char *str)
-{
-char subs[] = {'4', '3', '0', '7', '1'};
-char letters[] = {'a', 'e', 'o', 't', 'l'};
-for (int i = 0; str[i]; i++)
-{
-for (int j = 0; j < 5; j++)
-{
-if (str[i] == letters[j] || str[i] == letters[j] - 'a' + 'A')
-{
-str[i] = subs[j];
-break;
-}
-}
-}
-return (str);
-}
 
 /**
-* main - check the code
-* Return: Always 0.
-*/
-int main(void)
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
+char *leet(char *s)
 {
-char s[] =
-s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-char *p;
-p = leet(s);
-printf("%s", p);
-printf("%s", s);
-return (0);
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
+		}
+	}
+	return (s);
 }
