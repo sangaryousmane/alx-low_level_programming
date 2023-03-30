0650b5f50f51b0a510219b95ee0fa0ac776ac444
sangaryousmane <sangaryousmane@gmail.com>
2023-03-30 11:23:59 -0700

    Added  amin.c

---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char s[] =
+s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+char *p;
+
+p = leet(s);
+printf("%s", p);
+printf("%s", s);
+return (0);
+}
