1486f506bbc1156cef029ac29a208e1771e5d58d
sangaryousmane <sangaryousmane@gmail.com>
2023-03-30 11:29:13 -0700

    added main.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -22,3 +22,18 @@ break;
 }
 return (str);
 }
+
+/**
+* main - check the code
+* Return: Always 0.
+*/
+int main(void)
+{
+char s[] =
+s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+char *p;
+p = leet(s);
+printf("%s", p);
+printf("%s", s);
+return (0);
+}
