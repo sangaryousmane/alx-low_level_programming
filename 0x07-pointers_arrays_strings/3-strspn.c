8cc21255b1a74a065e99399f5f7da7f36dffbc9e
sangaryousmane <sangaryousmane@gmail.com>
2023-04-03 16:49:07 -0700

    Added 3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strspn - returns the by counts in @s that is alson in @accept
+* @s: first pointer
+* @accept: second pointer
+* Return: return the length of the count
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int bytes = 0;
+int i, j;
+
+for (i = 0; s[i] > '\0'; i++)
+{
+for (j = 0; accept[j] > '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+bytes++;
+break;
+}
+}
+if (!accept[i])
+{
+break;
+}
+return (bytes);
+}
