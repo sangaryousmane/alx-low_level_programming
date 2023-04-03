3d940bb178eb7f25c1720ddc5c8fdd9c2a740bcf
sangaryousmane <sangaryousmane@gmail.com>
2023-04-03 07:44:02 -0700

    Modified

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,8 +9,8 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int count = 0;
-for (; count < n; count++)
+unsigned int count;
+for (count = 0; count < n; count++)
 {
 s[count] = b;
 }
