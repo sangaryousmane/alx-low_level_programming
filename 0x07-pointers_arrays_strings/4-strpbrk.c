9926e6bccc34b4a0b17ed7ebb85c8fb6f2aa1d3d
sangaryousmane <sangaryousmane@gmail.com>
2023-04-04 09:04:31 -0700

    Added strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strpbrk - searches a string for set of bytes
+* @s: first pointer
+* @accept: second pointer
+* Return: a pointer to the byte in @s, otherwise, NULL
+*/
+char *_strpbrk(char *s, char *accept)
+{
+if (*s == '\0')
+{
+return (NULL);
+}
+if (*accept == '\0')
+{
+return (_strpbrk(s + 1, accept));
+}
+if (*s == *accept)
+{
+return (s);
+}
+return (_strpbrk(s, accept + 1));
+}
