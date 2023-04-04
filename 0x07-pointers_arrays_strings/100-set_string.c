7260acc8eaa6bce6ee453ccc7b293d4c8b9df904
sangaryousmane <sangaryousmane@gmail.com>
2023-04-03 19:34:50 -0700

    Double pointer is fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* set_string - sets the value of a pointer to a char
+* @s: a double pointer
+* @to: single pointer
+* Return: nothing
+*/
+void set_string(char **s, char *to)
+{
+**set = *to;
+}
