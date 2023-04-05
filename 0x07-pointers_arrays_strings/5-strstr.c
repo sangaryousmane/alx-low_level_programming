e52d1532e1d2881171e492f20763a22212102c09
sangaryousmane <sangaryousmane@gmail.com>
2023-04-04 20:22:04 -0700

    Resolved error in 5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,23 +8,35 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-if (*needle == '\0')
+int len1 = 0;
+while (haystack[len1] != '\0')
 {
-return (haystack);
+len1++;
 }
-while (*haystack != '\0')
-{
-if (*haystack == *needle)
-{
-char *p;
-p = _strstr(haystack + 1, needle + 1);
 
-if (p != NULL)
+int len2 = 0;
+while (needle[len2] != '\0')
 {
-return (haystack);
+len2++;
+}
+int i = 0;
+for (; i <= len1 - len2; i++)
+{
+int j = 0;
+while (j < len2)
+{
+if (haystack[i + j] != needle[j])
+{
+break;
+}
+j++;
+}
+if (j == len2)
+{
+return (&str1[i]);
 }
 }
-haystack++;
+return ('\0');
 }
-return (NULL);
-}
+
+
