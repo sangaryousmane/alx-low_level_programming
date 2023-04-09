c92213a0d75a757eee8328a80139a717a1575853
sangaryousmane <sangaryousmane@gmail.com>
2023-04-08 23:51:46 -0700

    Redo this, a very old task in the first sprint

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,42 +1,27 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* _strstr - locates a substring
+* _strstr - search for a substring in a string
 * @haystack: the string to search
 * @needle: first occurence to find
 * Return: pointer to the beginning of string
 */
 char *_strstr(char *haystack, char *needle)
 {
-int len1 = 0;
-while (haystack[len1] != '\0')
+while (*haystack != '\0')
 {
-len1++;
-}
-
-int len2 = 0;
-while (needle[len2] != '\0')
+char *i = haystack;
+char *p = needle;
+while (*i == *p && *p != '\0')
 {
-len2++;
+i++;
+p++;
 }
-int i = 0;
-for (; i <= len1 - len2; i++)
+if (*p == '\0')
 {
-int j = 0;
-while (j < len2)
-{
-if (haystack[i + j] != needle[j])
-{
-break;
+return (haystack);
 }
-j++;
+haystack++;
 }
-if (j == len2)
-{
-return (&str1[i]);
+return (0);
 }
-}
-return ('\0');
-}
-
 
