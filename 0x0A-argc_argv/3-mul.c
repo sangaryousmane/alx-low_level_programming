52ffb14eaa6735ffc18a06e24faae3351aaa132f
sangaryousmane <sangaryousmane@gmail.com>
2023-04-10 01:49:20 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - multiples two numbers and print the product
+* @argc: commandline count
+* @argv: arguments pass
+* Return: zerro is successful
+*/
+int main(int argc, int *argv)
+{
+int i = 1;
+int product = 0;
+if (argc < 2)
+{
+printf("Error\n");
+return (1);
+}
+product = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", product);
+return (0);
+}
