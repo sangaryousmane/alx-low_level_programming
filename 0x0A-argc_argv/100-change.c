b3b63877dabf3c9e32eceb1ca5757458cfb5c8a0
sangaryousmane <sangaryousmane@gmail.com>
2023-04-10 13:49:03 -0700

    Write a program that prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,24 +9,43 @@
  */
 int main(int argc, char *argv[])
 {
-int coins[5] = {25, 10, 5, 2, 1};
-int amount = atoi(argv[1]);
-int numCoins = 0;
-int i;
-if (argc != 2)
-{
-printf("%s\n", "Error");
-return (1);
-}
+	int cents, coins = 0;
 
-for (i = 0; i < 5; i++)
-{
-while (amount >= coins[i])
-{
-amount -= coins[i];
-numCoins++;
-}
-}
-printf("%d\n", numCoins);
-return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
 }
