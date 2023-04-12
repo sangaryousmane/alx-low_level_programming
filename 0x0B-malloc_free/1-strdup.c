2f64b0c5214a84ac55437aeea6576eba9cb334ba
sangaryousmane <sangaryousmane@gmail.com>
2023-04-12 14:04:10 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,21 @@
 #include <stdlib.h>
+
+/**
+* _strlen - return the string length
+* @s: the string of characters
+* Return: lenght of the string
+*/
+int _strlen(char *s)
+{
+int i = 0, length;
+while (s[i] != '\0')
+{
+length += 1;
+i++;
+}
+return (length);
+}
+
 /**
 * _strdup - returns a pointer to a copy of the new string
 * @str: the existing string
@@ -6,19 +23,25 @@
 */
 char *_strdup(char *str)
 {
-char duplicate[];
+int i = 0;
+char *duplicate;
 if (str == NULL)
 {
 return (NULL);
 }
-
-duplicate = malloc(sizeof(char) * 3);
+int length = _strlen(str);
+duplicate = malloc(sizeof(char) (length + 1);
 
 if (duplicate == NULL)
 {
 return (NULL);
 }
-duplicate = str;
+
+for (; i < length; i++)
+{
+duplicate[i] = str[i];
+}
+duplicate[i] = '\0';
 free(duplicate);
 return (duplicate);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int _putchar(int c);
+int _strlen(char *s);
 char *create_array(unsigned int size, char c);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
