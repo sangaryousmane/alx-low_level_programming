e9b59b14adedc865d7b76bb05757164a821b0e4a
sangaryousmane <sangaryousmane@gmail.com>
2023-04-19 21:43:05 -0700

    Write a function that searches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+* int_index - searches for an integer.
+* @array: array to search
+* @size: size of an array
+* @cmp: function to compare
+* Return: index of the element
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i = 0;
+
+if (size > 0)
+{
+if (array != NULL && cmp != NULL)
+{
+for (; i<size; i++)
+{
+if (cmp(array[i]))
+{
+return i;
+}
+}
+}
+}
+return -1;
+}
