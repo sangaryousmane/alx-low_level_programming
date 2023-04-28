aadc01346053a931f32e9d51518b26aa7789c12c
sangaryousmane <sangaryousmane@gmail.com>
2023-04-27 20:07:44 -0700

    Added a program that prints the file from which the code is compile

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - print the file named from which the code is compiled
+* Return: Always return 0 as success
+*/
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
