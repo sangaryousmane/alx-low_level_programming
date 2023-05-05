a497242301c2e2115c4ee034d7e2b0def6609e1c
sangaryousmane <sangaryousmane@gmail.com>
2023-05-05 08:18:29 -0700

    Solve get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,21 +4,22 @@
 * get_bit - get the value of a bit at a given index.
 * @n: value
 * @index: the index
-* Returns: the value of the bit at index index or -1 if an error occured
+* Return: the value of the bit at index index or -1 if an error occured
 */
-int get_bit(unsigned long int n, unsigned int index) {
+int get_bit(unsigned long int n, unsigned int index)
+{
 if (n == 0 && index < 64)
 {
-return 0;
+return (0);
 }
 while (index <= 63 && n != 0)
 {
 if (index == 0)
 {
-return n & 1;
+return (n & 1);
 }
 n >>= 1;
 index--;
 }
-return -1;
+return (-1);
 }
