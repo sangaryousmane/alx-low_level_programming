4932c1d317e295ef231cdfcd4fe56d9c1a27c37d
sangaryousmane <sangaryousmane@gmail.com>
2023-05-08 06:21:03 -0700

    Solved task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,24 +8,41 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-
-int code, write_file;
-if (!filename)
+int code, write_;
+int length = 0;
+if (filename == NULL)
 {
 return (-1);
 }
-code = open(filename, O_WRONLY | O_TRUNC, 600);
-if (code == -1)
+code = open(filename, O_WRONLY | O_TRUNC, 0600);
+if (code < 0)
 {
-write(code, "fails", sizeof("fails"));
 return (-1);
 }
-write_file = write(code, text_content, sizeof(text_content));
+if (text_content != NULL)
+{
+while (*(text_content + length))
+{
+length++;
+for (int i = 0; i < length; i++)
+{
+switch (*(text_content + i))
+{
+case '\n':
+write_ = write(code, "\n", 1);
+break;
+default:
+write_ = write(code, text_content + i, 1);
+break;
+}
+if (write_ < 0)
+{
 close(code);
-
-if (write_file < 0)
-{
 return (-1);
 }
+}
+}
+}
+close(code);
 return (1);
 }
