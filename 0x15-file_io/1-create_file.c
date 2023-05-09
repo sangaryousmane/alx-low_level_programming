a74d74b043c5d85e6301c089b83f200179d4133e
sangaryousmane <sangaryousmane@gmail.com>
2023-05-08 18:56:24 -0700

    Fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,6 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
+unsigned int t;
 fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd < 0)
 {
@@ -16,7 +17,7 @@ return (-1);
 }
 if (text_content)
 {
-int t = write(fd, text_content, strlen(text_content));
+t = write(fd, text_content, strlen(text_content));
 if (t != strlen(text_content))
 {
 return (-1);
