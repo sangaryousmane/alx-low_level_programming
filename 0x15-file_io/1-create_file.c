dd474e6d4f7878e8ce4f727b581123f0950f5df6
sangaryousmane <sangaryousmane@gmail.com>
2023-05-08 18:09:57 -0700

    Add task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
 * create_file - create new file
 * @filename: new file
@@ -8,28 +8,20 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int file_handler, t, length = 0;
-int i;
-if (!filename)
-return (-1);
-file_handler = open(filename, 0x0100 | 0x0002 | 0x0200, 0600);
-if (file_handler < 0)
+int fd;
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd < 0)
 {
 return (-1);
 }
 if (text_content)
 {
-for (i = 0; *(text_content + i); i++)
+int t = write(fd, text_content, strlen(text_content));
+if (t != strlen(text_content))
 {
-length++;
-}
-t = write(file_handler, text_content, length);
-if (t != length)
-{
-close(file_handler);
 return (-1);
 }
 }
-close(file_handler);
+close(fd);
 return (1);
 }
