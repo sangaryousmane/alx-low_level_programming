0300ab2f97aabaae319f5393bd8ff416415b9d73
sangaryousmane <sangaryousmane@gmail.com>
2023-05-09 21:03:36 -0700

    Solve task 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,35 +5,41 @@
 * @filename: name of the file
 * @letters: the number of letters it should read and print
 * Return: number of letters it could print
+*
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buffer;
-ssize_t code, write_to, read_from;
+int code;
+ssize_t read_file, write_file;
+char *buf;
+if (filename == NULL)
+{
+return (0);
+}
 code = open(filename, O_RDONLY);
-if (code)
+if (code < 0)
 {
 return (0);
 }
-buffer = malloc(sizeof(char) * letters);
-if (!buffer)
+buf = malloc(sizeof(char) * letters);
+if (!buf)
 {
 return (0);
 }
-read_from = read(code, buffer, letters);
-if (read_from)
+read_file = read(code, buf, letters);
+if (read_file < 0)
 {
-free(read_from);
+free(buf);
 return (0);
 }
-write_to = write(STDOUT_FILENO, buffer, read_from);
-
-if (write_to)
-{
-free(write_to);
-return (0);
-}
-free(buffer);
+buf[read_file] = '\0';
 close(code);
-return (write_to);
+write_file = write(STDOUT_FILENO, buf, read_file);
+if (write_file < 0)
+{
+free(buf);
+return (0);
+}
+free(buf);
+return (write_file);
 }
