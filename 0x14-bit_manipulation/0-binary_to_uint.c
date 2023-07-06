566ccd3192191ea933eb642fc502adb0250d0f10
sangaryousmane <sangaryousmane@gmail.com>
2023-07-06 05:30:45 -0700

    Solve binary to integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+* binary_to_uint - converts a binary to an unsigned int
+* @b: the binary num
+* Return: the converted integer
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimalNum = 0;
+	int i;
+
+	if (b != NULL)
+	{
+		for (i = 0; b[i] != '0'; i++)
+		{
+			if (b[i] != '0' && b[i] != '1')
+			{
+				return (0);
+			}
+			else
+			{
+				decimal = (decimal * 2) + (b[i] - '0');
+			}
+		}
+	}
+	else
+	{
+		return (0);
+	}
+	return (decimal);
+
+}
+
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,9 @@
+#ifndef __MAIN_
+#define __MAIN_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* __MAIN_ */
