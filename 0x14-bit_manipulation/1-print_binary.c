1fe95c60b639e4ec1050251c49ed91feb6d5d3f8
sangaryousmane <sangaryousmane@gmail.com>
2023-07-06 05:49:45 -0700

    Print binary numbers

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+* print_binary - print binary with requirements
+* @n: the number to print
+* Return: nothing
+*/
+void print_binary(unsigned long int n)
+{
+	if (!n)
+	{
+		_putchar('0');
+		return;
+	}
+	unsigned long  mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+
+	int isFalse = 0;
+
+	while (mask)
+	{
+		if (n & mask)
+		{
+			_putchar('1');
+			isFalse = 1;
+		}
+		else if (isFalse)
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
+	}
+
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,7 @@
 #include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
+int _putchar(char ch);
+void print_binary(unsigned long int n);
 
 #endif /* __MAIN_ */
