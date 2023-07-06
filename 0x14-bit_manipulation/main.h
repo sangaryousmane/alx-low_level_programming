0f5c8c5f286b003f703e1dc64bd34d0bff7a7e43
sangaryousmane <sangaryousmane@gmail.com>
2023-07-06 10:17:28 -0700

    Clear bits

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,4 +11,5 @@ void print_binary(unsigned long int n);
 int get_endianness(void);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif /* __MAIN_ */
