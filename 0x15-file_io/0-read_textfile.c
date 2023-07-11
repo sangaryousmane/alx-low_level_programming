ddfa96c55c6861444fc3b33435aee54bf4f06d47
sangaryousmane <sangaryousmane@gmail.com>
2023-07-11 04:09:35 -0700

    Fix code for task 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,32 +2,38 @@
 
 
 /**
-* read_text_file - read and print a text file to std output
+* read_textfile - read and print a text file to std output
 * @filename: is the file's name
 * @letters: number of letters to read and print
 * Return: 0 if filename is NULL or if it fails
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int code;
 	char *b;
-	ssize_t reader, writer;
+	ssize_t reader, writer, opener;
 
 	if (!filename)
 	{
 		return (0);
 	}
-	code = open(filename, O_RDONLY);
-	b = malloc(sizeof(char) * (letters));
+	opener = open(filename, O_RDONLY);
+	b = malloc(sizeof(char) * letters);
 
-	if (code == -1 || !b)
+	if (b == NULL)
 	{
 		return (0);
 	}
 
 	reader = read(code, b, letters);
 	writer = write(1, b, reader);
-	
+	if (reader == -1 || opener == -1)
+	{
+		if (writer != reader)
+		{
+			free(b);
+			return (0);
+		}
+	}
 	close(c);
 	free(b);
 	return (writer);
