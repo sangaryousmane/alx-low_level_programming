de379e170b2ec557d7d84820eb25160bc02044df
sangaryousmane <sangaryousmane@gmail.com>
2023-07-11 15:48:06 -0700

    Done task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,32 +9,26 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_descrpt, c;
-	int sz = 0;
 
-	if (!filename)
-	{
+	int o, w, len = 0;
+
+	if (filename == NULL)
 		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	file_descrpt = open(filename, O_WRONLY | O_APPEND);
-	if (file_descrpt < 0)
-	{
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
-	}
 
-	if (text_content)
-	{
-		while (text_content[sz])
-		{
-			sz++;
-		}
-		c = write(file_descrpt, text_content, sz);
-		if (c != sz)
-		{
-			return (-1);
-		}
-		close(file_descrpt);
-		return (1);
+	close(o);
 
+	return (1);
+}
 }
