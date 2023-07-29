eeb6bddf72bc861ddd2b7e01ffd34c2b473ddae4
sangaryousmane <sangaryousmane@gmail.com>
2023-07-29 07:45:10 -0700

    Fix truety

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		while (text_content[length] = '\0')
+		while (text_content[length] == '\0')
 		{
 			length++;
 		}
