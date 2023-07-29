35e5d12ef2be4a95b3057e5a0037f9e79983050d
sangaryousmane <sangaryousmane@gmail.com>
2023-07-29 07:47:56 -0700

    Fix code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,31 +10,27 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int c;
-	int length = 0, writer;
+	int fd, len, w;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	c = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (c == -1)
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (fd == -1)
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		while (text_content[length] == '\0')
-		{
-			length++;
-		}
-	writer = write(c, text_content, length);
+		for (len = 0; text_content[len] != '\0'; len++)
+		;
+		w = write(fd, text_content, len);
 	}
-	if (writer == -1)
+	if (w == -1)
 	{
 		return (-1);
 	}
-	close(c);
+	close(fd);
 	return (1);
 
 }
