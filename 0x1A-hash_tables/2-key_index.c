99f60301849f1b98dbdff82d81866dd850fd0225
sangaryousmane <sangaryousmane@gmail.com>
2023-09-07 22:05:23 -0700

    Use the djb2 algorithm to retrieve the key of a particular index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,16 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Get the index at which a key/value pair should
- *             be stored in array of a hash table.
- * @key: The key to get the index of.
- * @size: The size of the array of the hash table.
- *
- * Return: The index of the key.
- *
- * Description: Uses the djb2 algorithm.
+ * key_index - Gets the index of a key
+ * @key: The key that gives the index
+ * @size: The hash table array's size.
+ * Return: The key's index
  */
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
