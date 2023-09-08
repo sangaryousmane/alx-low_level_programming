1fbe24776e7ffd6e4300eee16b5dbfd9252357cc
sangaryousmane <sangaryousmane@gmail.com>
2023-09-07 22:00:51 -0700

    Implement the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -5,15 +5,15 @@
  * @str: The string to hash.
  * Return: The hash.
  */
-
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int count;
 
 	hash = 5381;
-	while (count = *str++)
+	while ((count = *str++))
+	{
 		hash = ((hash << 5) + hash) + count;
-
+	}
 	return (hash);
 }
