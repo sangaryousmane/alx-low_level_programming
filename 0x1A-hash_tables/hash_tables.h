bb37ef9906ff7c2e83f29607c096367de7cd0a30
sangaryousmane <sangaryousmane@gmail.com>
2023-09-09 20:13:05 -0700

    Add djb2 algorithm and implementation

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,7 +42,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-void swap(unsigned long int value, hash_node_t new_node, int index, hash_table_t *ht)
 /**
  * struct shash_node_s - Node of a sorted hash table
  * @key: The key, string
